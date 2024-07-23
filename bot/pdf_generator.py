2528825c349c13cf1bfe0edfd4e7d8a2fac92a97
AmirjonKarimov2007 <amirjonkarimoff@gmail.com>
2024-07-23 14:16:53 +0500

    deploy

--- bot/pdf_generator.py
+++ bot/pdf_generator.py
@@ -3,7 +3,6 @@ from reportlab.lib.units import mm
 from reportlab.pdfgen import canvas
 from datetime import datetime
 
-# Dictionary containing service names
 services = {
     1: {"name": "Service 1"},
     2: {"name": "Service 2"},
@@ -23,47 +22,40 @@ def create_pdf(output_path, services, ism_familiya, sharif,registrant):
                 f"{sharif}” GA TAKLIF ETILGAN XIZMATLAR\n"
                 "RO’YXATI")
     
-    # Draw the image
     c.drawImage(r"/home/amirjon/Telegram_Channels_Bot/Bytefy-Company-Telegram-Bot/bot/pdfconvertor.png", 0, 0, width, height)
     
-    # Set font for measuring and drawing text
     c.setFont("Helvetica-Bold", 12)
 
-    # Calculate the height offset for the header
-    header_y_offset = height - 115 * mm  # Adjust this as necessary to align with your template
-
-    # Draw the header
+    header_y_offset = height - 115 * mm  
     text_lines = sarlavha.split('\n')
     for line in text_lines:
         text_width = c.stringWidth(line, "Helvetica-Bold", 12)
-        header_x_offset = (width - text_width) / 2  # Center the text
+        header_x_offset = (width - text_width) / 2  
         c.drawString(header_x_offset, header_y_offset, line)
-        header_y_offset -= 14  # Move to the next line, adjust as necessary for line spacing
-
+        header_y_offset -= 14  
     # Draw the date
     now = datetime.now()
     day = now.day
     month = now.month
     year = now.year
     date = f"{day}/{month}/{year}"
-    c.setFont("Helvetica-Bold", 11)  # Set font size to 11 for the date
+    c.setFont("Helvetica-Bold", 11)  
     c.drawString(143, 450.5, date)
     # ============================================
-    c.setFont("Helvetica-Bold", 11)  # Set font size to 11 for the date
+    c.setFont("Helvetica-Bold", 11)  
     c.drawString(500, 450, '123456789')
-    # Coordinates for the table start
-    x_offset = 27 * mm  # Adjust this as necessary to align with your template
-    y_offset = 141 * mm  # Adjust this as necessary to align with your template
-    cell_height = 7.3 * mm  # Adjust this as necessary to align with your template
+    
+    x_offset = 27 * mm  
+    y_offset = 141 * mm  
+    cell_height = 7.3 * mm  
 
-    # Draw the table with service names only
-    c.setFont("Helvetica", 12)  # Reset font size for the table content if necessary
+    c.setFont("Helvetica", 12)  
     for i in range(1, 10):
         service_name = services[i]["name"] if i in services else ""
         c.drawString(x_offset, y_offset, service_name)
         y_offset -= cell_height
-    c.setFont("Helvetica-Bold", 12)  # Set font size to 11 for the date
-    c.drawString(423, 170, registrant)
+    c.setFont("Helvetica-Bold", 12)  
+    c.drawString(423, 175, registrant)
 
     c.save()
 
